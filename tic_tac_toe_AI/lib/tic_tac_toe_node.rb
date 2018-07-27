cee88b8547963cef9cd55192b0ee99c9546d4c21
Andre <fendred@yahoo.com>
2018-07-27 16:21:52 -0400

    Implemented initialize and deep_dup method

--- tic_tac_toe_AI/lib/tic_tac_toe_node.rb
+++ tic_tac_toe_AI/lib/tic_tac_toe_node.rb
@@ -1,7 +1,13 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +19,12 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    new_board = deep_dup(board)
+  end
+
+  private
+
+  def deep_dup(board)
+    board.map{|row| row.is_a?(Array) ? deep_dup(row) : row}
   end
 end
