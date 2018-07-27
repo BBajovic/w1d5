a5d81805d9f5c33d12fe119d93d32eafd0b7c44a
Andre <fendred@yahoo.com>
2018-07-27 09:31:17 -0400

    Pass rspec for initialize

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,26 @@
 class PolyTreeNode
+  attr_reader :parent, :children, :value
+
+  def initialize(value)
+      @parent = nil
+      @children = []
+      @value = value
+  end
+
+  # def parent
+  #
+  # end
+  #
+  # def children
+  #
+  # end
+  #
+  # def value
+  #
+  # end
+  #
+  # def parent=
+  #
+  # end
 
 end
