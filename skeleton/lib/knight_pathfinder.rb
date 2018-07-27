e3e5f0f494f9f666ed819bb1b4c3a6f4e8a6dace
Andre <fendred@yahoo.com>
2018-07-27 16:01:24 -0400

    Make tic_tac_toe folder

--- skeleton/lib/knight_pathfinder.rb
+++ skeleton/lib/knight_pathfinder.rb
@@ -3,14 +3,14 @@ require_relative "00_tree_node"
 class KnightPathFinder
 
   KNIGHT_MOVES = [
-    [-1, 2],
-    [ 1, 2],
-    [ 2, 1],
-    [ 2,-1],
-    [ 1,-2],
-    [-1,-2],
     [-2, 1],
-    [-2,-1]
+    [-2,-1],
+    [-1,-2],
+    [-1, 2],
+    [ 1,-2],
+    [ 1, 2],
+    [ 2,-1],
+    [ 2, 1]
   ]
 
   attr_reader :root_node, :previous_moves
@@ -18,19 +18,32 @@ class KnightPathFinder
   def initialize(start_pos)
     @root_node = PolyTreeNode.new(start_pos)
     @previous_moves = {start_pos => true}
+    build_move_tree
   end
+
+  def find_path(end_pos)
+    node = root_node.bfs(end_pos)
+
+    if node.nil?
+      "Invalid input"
+    else
+      trace_path_back(node)
+    end
+
+  end
+
+  private
 
   def build_move_tree
     line = []
     line << root_node
-
 
     until line.empty?
       current_position = line.shift
       valid_moves = get_valid_moves(current_position.value)
       valid_moves.each do |move|
         unless previous_moves.has_key?(move)
-          next_position = PolyTreeNode.new(move)
+          p next_position = PolyTreeNode.new(move)
           line << next_position
           previous_moves[move] = true
           next_position.parent = current_position
@@ -39,18 +52,10 @@ class KnightPathFinder
     end
   end
 
-  def find_path(end_pos)
-
-  end
-
-
-
   def trace_path_back(node)
     return [node.value] if root_node.value == node.value
-    trace_path_back(node.parent) << value
+    trace_path_back(node.parent) << node.value
   end
-
-  private
 
   def get_valid_moves(pos)
     x_idx, y_idx = pos
@@ -75,5 +80,4 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   k = KnightPathFinder.new([0,0])
-  k.build_move_tree
 end
